e0097962c985d99af1cf8eeb75dd263470b5545b
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-05-07 13:51:24 +0530

    initial-commit

---
+++ templates/index.js
@@ -0,0 +1,56 @@
+// document.querySelector(".card").addEventListener("click", function () {
+//   this.classList.toggle("open");
+//   const videoContainer = this.querySelector(".video-container");
+//   if (videoContainer.classList.contains("show")) {
+//     const video = this.querySelector("video");
+//     if (video.paused) {
+//       video.play();
+//     } else {
+//       video.pause();
+//     }
+//   }
+//   videoContainer.classList.toggle("show");
+// });
+
+// if we want to put the secret code feature later:
+
+const secretCode = "8877";
+let codeEntered = false;
+
+function showVideo() {
+  document.querySelector(".card").classList.add("open");
+  document.querySelector(".video-container").classList.add("show");
+}
+
+function checkCode() {
+  const code = prompt("Please enter your code of happiness:");
+
+  if (code === secretCode) {
+    codeEntered = true;
+    showVideo();
+  } else {
+    alert("Incorrect code. Please try again.");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  checkCode();
+
+  document.querySelector(".card").addEventListener("click", function () {
+    if (!codeEntered) {
+      alert("Please enter the secret code to access the video.");
+      return;
+    }
+
+    const videoContainer = this.querySelector(".video-container");
+    if (videoContainer.classList.contains("show")) {
+      const video = this.querySelector("video");
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }
+    videoContainer.classList.toggle("show");
+  });
+});
